975f0bf86742a8cf0591e0139bbaf673ef3f8aac
eleving-bi <126457844+eleving-bi@users.noreply.github.com>
2023-10-22 15:27:11 +0300

    Update insert_rows.sql
    SELECT + UNION ALL

--- macros/utils/table_operations/insert_rows.sql
+++ macros/utils/table_operations/insert_rows.sql
@@ -27,7 +27,7 @@
     {% elif insert_rows_method == 'chunk' %}
       {% set rows_chunks = elementary.split_list_to_chunks(rows, chunk_size) %}
       {% for rows_chunk in rows_chunks %}
-        {% set insert_rows_query = elementary.get_chunk_insert_query(table_relation, columns, rows_chunk) %}
+        {% set insert_rows_query = get_chunk_insert_query(table_relation, columns, rows_chunk) %}
         {% do elementary.run_query(insert_rows_query) %}
       {% endfor %}
     {% else %}
@@ -39,6 +39,7 @@
     {% endif %}
 {% endmacro %}
 
+{# Using custom SELECT + UNION ALL instead of INSERT INTO ... VALUES #}
 {% macro get_insert_rows_queries(table_relation, columns, rows, query_max_size=none, on_query_exceed=none) -%}
     {% if not query_max_size %}
       {% set query_max_size = elementary.get_config_var('query_max_size') %}
@@ -49,46 +50,51 @@
        insert into {{ table_relation }}
          ({%- for column in columns -%}
            {{- column.name -}} {{- "," if not loop.last else "" -}}
-         {%- endfor -%}) values
+         {%- endfor -%})
     {% endset %}
 
     {% set current_query = namespace(data=base_insert_query) %}
     {% for row in rows %}
       {% set row_sql = elementary.render_row_to_sql(row, columns) %}
-      {% set query_with_row = current_query.data + ("," if not loop.first else "") + row_sql %}
-
-      {% if query_with_row | length > query_max_size %}
-        {% set new_insert_query = base_insert_query + row_sql %}
-
-        {# Check if row is too large to fit into an insert query. #}
-        {% if new_insert_query | length > query_max_size %}
+        
+      {% set query_with_row = current_query.data %}
+        {% if loop.first %}
+            {% set query_with_row = query_with_row + ' SELECT ' + row_sql %}
+        {% else %}
+            {% set query_with_row = query_with_row + ' UNION ALL SELECT ' + row_sql %}
+        {% endif %}
+        
+        {% if query_with_row | length > query_max_size %}
           {% if on_query_exceed %}
             {% do on_query_exceed(row) %}
             {% set row_sql = elementary.render_row_to_sql(row, columns) %}
-            {% set new_insert_query = base_insert_query + row_sql %}
+            {% set new_insert_query = base_insert_query + ' SELECT ' + row_sql %}
           {% endif %}
-
+            
           {% if new_insert_query | length > query_max_size %}
             {% do elementary.file_log("Oversized row for insert_rows: {}".format(query_with_row)) %}
             {% do exceptions.raise_compiler_error("Row to be inserted exceeds var('query_max_size'). Consider increasing its value.") %}
           {% endif %}
-
+            
           {% if current_query.data != base_insert_query %}
-            {% do insert_queries.append(current_query.data) %}
-          {% endif %}
-          {% set current_query.data = new_insert_query %}
+                {% do insert_queries.append(current_query.data) %}
+            {% endif %}
+            
+            {% set current_query.data = new_insert_query %}
+        {% else %}
+            {% set current_query.data = query_with_row %}
         {% endif %}
-      {% else %}
-        {% set current_query.data = query_with_row %}
-      {% endif %}
-      {% if loop.last %}
-        {% do insert_queries.append(current_query.data) %}
-      {% endif %}
+        
+        {% if loop.last %}
+            {% do insert_queries.append(current_query.data) %}
+        {% endif %}
     {% endfor %}
 
     {{ return(insert_queries) }}
+
 {%- endmacro %}
 
+{# Removed brackets #}
 {% macro render_row_to_sql(row, columns) %}
   {% set rendered_column_values = [] %}
   {% for column in columns %}
@@ -100,23 +106,25 @@
       {% do rendered_column_values.append(elementary.render_value(column_value)) %}
     {% endif %}
   {% endfor %}
-  {% set row_sql = "({})".format(rendered_column_values | join(",")) %}
+  {% set row_sql = "{}".format(rendered_column_values | join(",")) %} {# Removed brackets #}
   {% do return(row_sql) %}
 {% endmacro %}
 
+{# Using custom SELECT + UNION ALL instead of INSERT INTO ... VALUES #}
 {% macro get_chunk_insert_query(table_relation, columns, rows) -%}
     {% set insert_rows_query %}
         insert into {{ table_relation }}
             ({%- for column in columns -%}
                 {{- column.name -}} {{- "," if not loop.last else "" -}}
-            {%- endfor -%}) values
-            {% for row in rows -%}
-                ({%- for column in columns -%}
-                    {%- set column_value = elementary.insensitive_get_dict_value(row, column.name, none) -%}
-                    {{ elementary.render_value(column_value) }}
-                    {{- "," if not loop.last else "" -}}
-                 {%- endfor -%}) {{- "," if not loop.last else "" -}}
+            {%- endfor -%})
+        {% for row in rows -%}
+            {{- ' SELECT ' if loop.first else ' UNION ALL SELECT ' -}}
+            {%- for column in columns -%}
+                {%- set column_value = elementary.insensitive_get_dict_value(row, column.name, none) -%}
+                {{ elementary.render_value(column_value) }}
+                {{- "," if not loop.last else "" -}}
             {%- endfor -%}
+        {%- endfor %}
     {% endset %}
     {{ return(insert_rows_query) }}
 {%- endmacro %}
