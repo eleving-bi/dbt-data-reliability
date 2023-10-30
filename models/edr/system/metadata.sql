1b74b04021083de7e0125382310df9613e6b7929
eleving-bi <126457844+eleving-bi@users.noreply.github.com>
2023-10-30 11:42:37 +0200

    Update metadata.sql
    Column as string

--- models/edr/system/metadata.sql
+++ models/edr/system/metadata.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
 SELECT
-    '{{ elementary.get_elementary_package_version() }}' as dbt_pkg_version
+    {{ const_as_string(elementary.get_elementary_package_version()) }} as dbt_pkg_version
